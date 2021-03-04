be301e36be9d11ac96ca1e192b5f20486bd6bc76
felipeafbruno <felipedobru@gmail.com>
2021-03-04 10:44:56 -0300

    Exemplo simples de Gráfico de dispersão

--- visualização_dados_python/graficos_dispersao.py
+++ visualização_dados_python/graficos_dispersao.py
@@ -12,8 +12,8 @@ plt.title(titulo)
 plt.xlabel(eixoX)
 plt.ylabel(eixoY)
 
-plt.scatter(x, y, label='Meus pontos', color='red')
-plt.plot(x, y)
+plt.scatter(x, y, label='Meus pontos', color='g', marker='.', s=100)
+plt.plot(x, y, color='k', linestyle=':')
 # legend() - faz com que a legenda aparece no gráfico
 plt.legend()
 
