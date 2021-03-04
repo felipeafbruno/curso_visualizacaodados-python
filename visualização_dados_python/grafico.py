d51f0e8138ee351d832ca26474294d024f7b305b
felipeafbruno <felipedobru@gmail.com>
2021-03-04 10:10:08 -0300

    Exemplo simples de Gráfico de Linha com Matplotlib

---
+++ visualização_dados_python/grafico.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt 
+
+
+x = [1, 2, 5]
+y = [2, 3, 7]
+
+# Título
+plt.title('Meu primeiro gráfico com Python')
+
+# Eixos
+plt.xlabel('Eixo X')
+plt.ylabel('Eixo Y')
+
+plt.plot(x, y)
+plt.show()
